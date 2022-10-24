556b71513c4b8161026351880809f98b792143bc
Ethan Davidson <ethanld@outlook.com>
2022-10-24 13:05:36 -0700

    renderAll funct

--- script.js
+++ script.js
@@ -3,9 +3,16 @@ window.addEventListener("DOMContentLoaded", () => {
 }
 
 function fetchCrypto() {
-  fetch(`http://localhost:3000/crypto`)
+  fetch(`http://localhost:3000/crypto?q=${searchTerm}`)
   .then(r => r.json())
   .then(data => {
     console.log(data)
 
+    crypto.innerHTML = renderAll(data)
+
+})
+}
+
+function renderAll(crypto) {
+  return crypto.map(c => renderSingle(c)).join("")
 }
