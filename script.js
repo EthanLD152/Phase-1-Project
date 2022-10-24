7cb53de1569367e5e36fa86186dce4812e01b99e
Ethan Davidson <ethanld@outlook.com>
2022-10-24 15:44:21 -0700

    changing like

--- script.js
+++ script.js
@@ -49,10 +49,12 @@ const btn = document.querySelector(".crypto-like-button")
   const heart = e.target;
   const EMPTY_HEART = '♡'
   const FULL_HEART = '♥'
-crypto.forEach((crypto) => {
-      heart.innerText === FULL_HEART
+  if(heart.className === "like-glyph"){
       heart.className = 'liked-heart'
-})
+  } else if (heart.className === "liked-heart"){
+    heart.innerText === EMPTY_HEART
+    heart.className = "unliked-heart"
+  } else if (heart.className === "unliked-heart")
     }
 
   for (const glyph of allHearts) {
