4ca5210021820d45a310066bd438f765fbee2179
Ethan Davidson <ethanld@outlook.com>
2022-11-01 15:47:45 -0700

    adding search

--- script.js
+++ script.js
@@ -1,66 +1,91 @@
 window.addEventListener("DOMContentLoaded", () => {
   fetchCrypto()
-  document.querySelector("#search").addEventListener("submit", search)
 })
 
+const cryptoId = document.querySelector("#cryptoId")
+const cryptoContainer = document.querySelector("#container")
+const cryptoPrice = document.querySelector("#price")
+const cryptoRate = document.querySelector("#rate")
+const searchContainer = document.querySelector("#searchContainer")
 
-const crypto = document.querySelector("#container")
+
+
 function fetchCrypto() {
+  fetch(`http://localhost:3000/crypto`)
+    .then(r => r.json())
+    .then(data => {
+        data.map(cryptoInfo => {
+          const h1 = document.createElement("h1")
+          h1.textContent = cryptoInfo.name
+
+          const h2 = document.createElement("h2")
+          h2.textContent = cryptoInfo.price
+
+          const h3 = document.createElement("h3")
+          h3.textContent = cryptoInfo.dropRate
+
+          const newBtn = document.createElement("button")
+          newBtn.textContent = "like ♡"
+          handleLikeFunc(newBtn)
+          search(cryptoInfo, h1, h2, h3)
+
+          cryptoContainer.append(h1)
+          cryptoContainer.append(h2, h3, newBtn)
+        })
+      })
+}
+
+function handleLikeFunc(newBtn) {
+  newBtn.addEventListener("click", function () {
+    if(newBtn.textContent === "like ♡") {
+      newBtn.textContent = 'like ♥'
+    } else if(newBtn.textContent === "like ♥") {
+      newBtn.textContent = "like ♡"
+    }
+     })
+}
+
+const div = document.createElement("div");
+
+
+
+function search(cryptoInfo, h1, h2, h3) {
+const turnArray = Object.entries(cryptoInfo)
+
   const form = document.querySelector("#search");
+
   form.addEventListener('submit', (e) => {
     e.preventDefault()
     const searchTerm = document.querySelector("#id").value
     document.querySelector("#id").value = ""
-    const likeBtn = document.querySelector('.crypto-like-button')
-    likeBtn.style.display = 'block'
-  fetch(`http://localhost:3000/crypto?q=${searchTerm}`)
-  .then(r => r.json())
-  .then(data => {
-    console.log(data)
 
-    crypto.innerHTML = renderAll(data)
+    const find = turnArray.find((cryptoInfo) => {
+      console.log(cryptoInfo.name === searchTerm)
+
+    })
   })
-  })
-}
-
-function renderAll(crypto) {
-  return crypto.map(c => renderSingle(c)).join("")
-}
-
-function renderSingle(crypto){
-  return `<div class="crypto-card" id="${crypto.id}">
-    <div class="crypto-frame">
-      <h1 class="center-text">${crypto.name}</h1>
-      <h2>Price(usd) - $${crypto.price} </h2>
-      <h2>DropRate - ${crypto.dropRate} </h2>
-    </div>
-  </div>`
-
-};
-
-
-const allHearts = document.querySelectorAll('.like-glyph');
-
-
-const btn = document.querySelector(".crypto-like-button")
 
 
 
-  function like(e) {
-  const heart = e.target;
-  const EMPTY_HEART = '♡'
-  const FULL_HEART = '♥'
-  if(heart.className === "like-glyph"){
-      heart.className = 'liked-heart'
-  } else if (heart.className === "liked-heart"){
-    heart.className = "unliked-heart"
-  } else if (heart.className === "unliked-heart") {
-    heart.className = "liked-heart"
   }
-    }
 
-  for (const glyph of allHearts) {
-  glyph.addEventListener("click", like)
-  }
+// console.log(cryptoContainer.find(search))
+
+
+//.find method to grab cypto object i need
+
+//create div element for each crypto and append(prefer)
+
+//descriptive names
+
+//11 - 24 call bacl extracted to seperate function
+
+//spacing indentation
+//append to cryptoContainer/ use the variable to access dynamic values
+
+
+
+
+
 
 
